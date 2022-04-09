d6329a994563cb01f7128da9a928ea3079160291
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-09 23:29:56 +0600

    react firebase hooks implement

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
+import Register from './components/Register/Register';
+import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element ={<Home></Home>}></Route>
         <Route path='/login' element ={<Login></Login>}></Route>
+        <Route path= '/register' element ={<Register></Register>}></Route>
+        <Route path='/products' element ={<Products></Products>}></Route>
+        <Route path='/orders' element ={<Orders></Orders>}></Route>
       </Routes>      
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,17 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
+import app from '../../firebase.init';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const auth = getAuth(app);
 
 const Products = () => {
+    const [user] = useAuthState(auth);
+
     return (
         <div>
             <h3>This is product components.</h3>
+            <h4>{user ? user.displayName : "No body"}</h4>
         </div>
     );
 };
